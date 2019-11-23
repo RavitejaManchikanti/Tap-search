ce2262bfae789b1f49ad25615ce8db918d84809e
RavitejaManchikanti <40654233+RavitejaManchikanti@users.noreply.github.com>
2019-11-23 23:13:27 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+       name=='Tap-search'
+       version='1.0'
+       description='Testing Texract application'
+       author='Raviteja Manchikanti'
+       author_email="raviteja.3561@gmail.com"
+       packages=["Tap-search"]
+       install_requirements=['Texract']
